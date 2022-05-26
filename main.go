07fe269017cc891f3e2f18afc93dd0c910cb1b87
Hikaru Inomata <inomatahi@icloud.com>
2022-05-26 03:58:36 +0000

    get id

--- main.go
+++ main.go
@@ -25,6 +25,18 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 
@@ -39,6 +51,7 @@ func postAlbums(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
