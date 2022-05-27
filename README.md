075ea80969389afc380a9b8c7be0f0698658bcef
Hikaru Inomata <inomatahi@icloud.com>
2022-05-27 00:23:01 +0000

    Post to Dynamodb

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 参考資料
 - https://go.dev/doc/tutorial/web-service-gin
   - Goが出してる公式のドキュメント
+- https://aws.github.io/aws-sdk-go-v2/docs/getting-started/
+  - AWS SDKのチュートリアル
--- main.go
+++ main.go
@@ -3,21 +3,26 @@ package main
 import (
 	"net/http"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/gin-gonic/gin"
 )
 
 type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Artist string `json:"artist"`
+	Price  string `json:"price"`
 }
 
 var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: "56.99"},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: "17.99"},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: "39.99"},
 }
+
+var ddb = dynamodb.New(session.New(), aws.NewConfig().WithRegion("ap-northeast-1"))
 
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
@@ -39,10 +44,34 @@ func postAlbums(c *gin.Context) {
 	var newAlbum album
 
 	if err := c.BindJSON(&newAlbum); err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "bind failed"})
 		return
 	}
 
-	albums = append(albums, newAlbum)
+	param1 := &dynamodb.PutItemInput{
+		TableName: aws.String("gin-tutorial-table"),
+		Item: map[string]*dynamodb.AttributeValue{
+			"id": {
+				N: aws.String(newAlbum.ID),
+			},
+			"title": {
+				S: aws.String(newAlbum.Title),
+			},
+			"artist": {
+				S: aws.String(newAlbum.Artist),
+			},
+			"price": {
+				N: aws.String(newAlbum.Price),
+			},
+		},
+	}
+	_, err := ddb.PutItem(param1)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
